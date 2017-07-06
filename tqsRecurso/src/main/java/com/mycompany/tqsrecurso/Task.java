a00afeb65dfb1557e59a0545032b03ed11289df1
Goncalo Pinto <goncalopinto@ua.pt>
2017-07-06 16:55:57 +0100

    actualizado com testes unitários

--- tqsRecurso/src/main/java/com/mycompany/tqsrecurso/Task.java
+++ tqsRecurso/src/main/java/com/mycompany/tqsrecurso/Task.java
@@ -23,7 +23,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author gonca
+ * @author gonca lasdsiodfjsdiofjds
  */
 @Entity
 @Table(name = "TASK")
--- tqsRecurso/target/generated-sources/annotations/com/mycompany/tqsrecurso/Task_.java
+++ tqsRecurso/target/generated-sources/annotations/com/mycompany/tqsrecurso/Task_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-06T14:57:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-06T16:54:52")
 @StaticMetamodel(Task.class)
 public class Task_ { 
 

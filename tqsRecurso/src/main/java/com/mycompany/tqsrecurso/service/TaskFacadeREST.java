b44bcb6347b770b3c4a113d299cad4dc37a34f85
Goncalo Pinto <goncalopinto@ua.pt>
2017-07-08 20:32:24 +0100

    Apenas vão ficar testes unitários e de desempenho (integração... fora)

--- tqsRecurso/src/main/java/com/mycompany/tqsrecurso/service/TaskFacadeREST.java
+++ tqsRecurso/src/main/java/com/mycompany/tqsrecurso/service/TaskFacadeREST.java
@@ -32,14 +32,14 @@ public class TaskFacadeREST extends AbstractFacade<Task> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void create(Task entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, Task entity) {
         super.edit(entity);
     }
@@ -52,21 +52,21 @@ public class TaskFacadeREST extends AbstractFacade<Task> {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public Task find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Task> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Task> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
